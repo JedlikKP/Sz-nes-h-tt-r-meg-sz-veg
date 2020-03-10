c41d2ea604852caebcd71aa71b7430e0eeb4dc1f
JedlikKP <60391171+JedlikKP@users.noreply.github.com>
2020-03-10 10:57:25 +0100

    Add files via upload

--- Zebracsikos kiíratás/Program.cs
+++ Zebracsikos kiíratás/Program.cs
@@ -4,12 +4,30 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Zebracsikos_kiíratás
+namespace Színes_háttér_meg_szöveg
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            //
+            // Színek és háttérszínek!
+            //
+            Type type = typeof(ConsoleColor);
+            Console.ForegroundColor = ConsoleColor.White;
+            foreach (var name in Enum.GetNames(type))
+            {
+                Console.BackgroundColor = (ConsoleColor)Enum.Parse(type, name);
+                Console.WriteLine(name);
+            }
+            Console.BackgroundColor = ConsoleColor.Black;
+            foreach (var name in Enum.GetNames(type))
+            {
+                Console.ForegroundColor = (ConsoleColor)Enum.Parse(type, name);
+                Console.WriteLine(name);
+                
+            }
+            Console.ReadKey();
         }
     }
 }
